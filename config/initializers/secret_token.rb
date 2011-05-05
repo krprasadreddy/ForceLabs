a18978bde87132ace65092195880d422250e9256
Harshit <mailtoharshit@gmail.com>
2011-05-05 17:33:32 -0400

    Second Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forcelabs::Application.config.secret_token = '4d4f400a5e3b972fa251532369585e8a7ae32ee8d0e3b470e6ce86c515f3c3d069f17e9d94374f419c2a25a734e15a230621a5c041da7dd7e2080ca510f3f417'
